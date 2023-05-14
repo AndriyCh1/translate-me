9fb5825281f01ca28b7f9ab37a67a6290cd7fa1a
AndriyCh1 <chekanovskiyacc@gmail.com>
2023-05-14 12:57:19 +0300

    feat: create theme

---
+++ client/src/styles/theme.ts
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    background: {
+      paper: "#01497c",
+    },
+    text: {
+      primary: "#f5f4f4",
+      secondary: "#46505A",
+    },
+    info: {
+      main: "#a9d6e5",
+    },
+  },
+});
