9bbba8f26b6b604f83a8c7e6e577cb1a257543df
AndriyCh1 <chekanovskiyacc@gmail.com>
2023-05-14 11:14:21 +0300

    feat: add navbar, set up routing

---
+++ client/src/components/common/navbar/navbar.tsx
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import MenuItem from "@mui/material/MenuItem";
+import { CssBaseline, styled } from "@mui/material";
+
+import { AppRoute } from "../../../common/enums";
+
+const pages = [
+  { display: "Exercises", route: AppRoute.EXERCISES },
+  { display: "Constructor", route: AppRoute.CONSTRUCTOR },
+  { display: "Stored", route: AppRoute.STORED },
+];
+
+const StyledLink = styled(Link)(() => ({
+  color: "#ffffff",
+  textDecoration: "none",
+  margin: "5px",
+}));
+
+const Navbar = () => {
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <CssBaseline />
+        <Toolbar disableGutters sx={{ padding: 0, margin: 0 }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page, index) => (
+                <MenuItem key={index} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">
+                    <StyledLink to={page.route} sx={{ color: "black" }}>
+                      {page.display}
+                    </StyledLink>
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page, index) => (
+              <Typography key={index}>
+                <StyledLink to={page.route}>{page.display}</StyledLink>
+              </Typography>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default Navbar;
